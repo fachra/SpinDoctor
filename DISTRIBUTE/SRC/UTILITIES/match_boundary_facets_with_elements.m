aa539b8663066032d0a878ebd29a68acc4635c42
Van Dang Nguyen <vdnguyen@84-217-80-43.customers.ownit.se>
2019-01-13 09:36:19 +0100

    fix minor misprints

--- DISTRIBUTE/SRC/UTILITIES/match_boundary_facets_with_elements.m
+++ DISTRIBUTE/SRC/UTILITIES/match_boundary_facets_with_elements.m
@@ -4,20 +4,20 @@ function boundaryTetgen2element=match_boundary_facets_with_elements(boundaryTetg
 %this part creates own boundary facets
 [elems2faces, faces2nodes]=get_faces(elements);
 faces2elems=entryInWhichRows(elems2faces); 
-bindex=find(faces2elems(:,2)==0);           %index of boundary faces - belong to one element only
-boundary=faces2nodes(bindex,:);             %nodes of boundary faces 
-boundary2element=faces2elems(bindex,1);     %element of boundary faces (unique)
+bindex=find(faces2elems(:,2)==0);           %index of boundary facets - belong to one element only
+boundary=faces2nodes(bindex,:);             %nodes of boundary facets 
+boundary2element=faces2elems(bindex,1);     %elements of boundary facets (unique)
 
-%oder all matrices columnwise for comparison
+%order all matrices columnwise
 boundary_ordered=sort(boundary,2);
 boundaryTetgen_ordered=sort(boundaryTetgen,2);
 
-%find their intersection
-[~,ind,indTetgen] = intersect(boundary_ordered,boundaryTetgen_ordered,'rows');
+%find their intersection (matching)
+[~,index,indexTetgen] = intersect(boundary_ordered,boundaryTetgen_ordered,'rows');
 
 %and assign (unique) elements to boundary facets 
 boundaryTetgen2element=zeros(size(boundaryTetgen,1),1);
-boundaryTetgen2element(indTetgen)=boundary2element(ind);  %a columnt vector
+boundaryTetgen2element(indexTetgen)=boundary2element(index);  %column vector
 
 end
 
